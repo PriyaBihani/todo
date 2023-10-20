1a908916f4774e1b724de981bfba9ff01ccf43a7
PriyaBihani <priyabihani.official@gmail.com>
2023-10-21 00:56:19 +0530

    Code for Destructring and JSON intro

--- src/index.js
+++ src/index.js
@@ -1,53 +1,33 @@
-const person = new Object(); // Object constructor syntax
-
-person.firstName = "John";
-person.lastName = "Doe";
-person.age = 50;
-
-console.log(person);
-
-const obj1 = { a: 1, b: 5, c: 10 };
-const obj2 = { d: 67, e: 89, f: 90 };
-
-// spread operator
-const obj3 = { ...obj1, ...obj2 };
-
-const obj4 = Object.assign({}, obj1, obj2);
-
-// Array of Objects
-const arr = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Doe" },
-  { id: 3, name: "Jane" },
-];
-
-let output = arr[1].name;
-
-output = Object.keys(person);
-
-output = Object.keys(person).length;
-
-output = Object.values(person);
-
-output = Object.entries(person);
-
-output = person.hasOwnProperty("isAdmin");
-
-output = "isAdmin" in person;
-
-console.log(output);
-
-// Naming
-
-const firstName = "Priya";
-const lastName = "Bihani";
-
-const person2 = {
-  firstName,
-  lastName,
-  greet() {
-    return `Hello ${this.firstName} ${this.lastName}`;
+const toDo = {
+  todo_id: 1,
+  title: "Create Videos",
+  user: {
+    name: "Priya Bihani",
   },
 };
-console.log(person2.greet());
-console.log(person2);
+
+// Converting the object to JSON string
+const str = JSON.stringify(toDo);
+console.log(str);
+
+// Converting the JSON string to object
+const obj = JSON.parse(str);
+console.log(obj);
+
+// Destructuring
+const {
+  todo_id,
+  title: todo_title,
+  user,
+  user: { name },
+} = toDo;
+
+console.log(todo_id, todo_title, name, user);
+
+// Arrays Destructuring
+
+const arr = [34, 56, 78, 98, 100];
+
+const [first, second, ...rest] = arr;
+
+console.log(first, second, rest);
