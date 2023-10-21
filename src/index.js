3be700a837637286912647e2ec2cd9743b067c31
PriyaBihani <priyabihani.official@gmail.com>
2023-10-21 22:58:09 +0530

    Code for primitive vs reference memory storage

--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
-let output;
+let name = "Priya";
+let age = 20;
 
-const fullName = "Priya bihani"; // primitive
-const age = 21;
+let person = {
+  name: "deepak",
+  age: 30,
+};
 
-output = "My name is " + fullName + ". and my age is " + age + " years old";
+let newName = name;
 
-// Template literals
-// output = `My name is ${fullName}. and my age is ${age} years old`;
+newName = "Kartik";
 
-// String constructor
-const s = new String("Hello World"); // reference type
+let newPerson = person;
+console.log(person, newPerson);
+newPerson.name = "Rahul";
 
-// console.log(typeof output, typeof s);
-
-// String methods
-
-output = s.length;
-output = fullName.length;
-
-output = s.toUpperCase();
-output = fullName.toUpperCase();
-
-output = s.toLowerCase();
-
-output = s[2];
-
-output = s.charAt(4);
-
-output = s.substring(0, 7);
-
-output = s.indexOf("W");
-output = s.indexOf("World");
-
-output = "           Hello world";
-output = output.trim();
-
-output = s.valueOf();
-
-output = s.split(" ");
-
-console.log(output);
+// console.log(name, newName);
+console.log(person, newPerson);
