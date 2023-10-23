e92ca601516d9dd50fc682fb2ac9283c00a05688
PriyaBihani <priyabihani.official@gmail.com>
2023-10-23 23:11:08 +0530

    Code if if else and switch case statement

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
-let name = "Priya";
-let age = 20;
+const x = 5;
+const y = 10;
 
-let person = {
-  name: "deepak",
-  age: 30,
-};
+// if (x === y) {
+//   console.log("x is equal to y");
+// } else if (x > y) {
+//   console.log("x is greater than y");
+// } else {
+//   console.log("x is less than y");
+// }
 
-let newName = name;
+// Nested if
+const score = 60;
 
-newName = "Kartik";
+if (score <= 50) {
+  console.log("You need to practice more");
+  if (score < 30) {
+    console.log("Failed");
+  } else {
+    console.log("Just Passed");
+  }
+} else if (score > 50 && score < 75) {
+  console.log("Keep pushing");
+}
 
-let newPerson = person;
-console.log(person, newPerson);
-newPerson.name = "Rahul";
+const color = "yellow";
 
-// console.log(name, newName);
-console.log(person, newPerson);
+// if (color === "red" || color === "blue" || color === "green") {
+//   console.log("Color is red or blue");
+// }
+
+// Switch case statement (===)
+
+switch (color) {
+  case "red":
+    console.log("Color is red");
+    break;
+  case "blue":
+    console.log("Color is blue");
+    break;
+  case "green":
+    console.log("Color is green");
+    break;
+  default:
+    console.log("COlor not supported");
+}
+
+// console.log("This is a test");
