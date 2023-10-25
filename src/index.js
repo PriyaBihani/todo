a40c1cb493383bab7bcc3b5a910d2c8f88b92a3c
PriyaBihani <priyabihani.official@gmail.com>
2023-10-26 00:46:17 +0530

    Code scope in js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
-const cars = ["Ford", "Chevy", "Honda", "Toyota"];
-const users = [{ name: "Priya" }, { name: "Riya" }, { name: "Deepak" }];
-// for (let i = 0; i < cars.length; i++) {
-//   console.log(cars[i]);
-// }
-
-// for .... of loop
-// for (const item of cars) {
-//   console.log(item);
-// }
-
-for (const user of users) {
-  console.log(user.name);
+if (true) {
+  const y = 1;
+  console.log(y);
 }
 
-// for ... in loop
-const obj = {
-  color1: "red",
-  color2: "green",
-  color3: "blue",
-  color4: "yellow",
-};
+// console.log(y); // Reference error
 
-for (const key in obj) {
-  console.log(key, obj[key]);
+const x = 1;
+console.log(x, "in global scope");
+
+function run() {
+  // Access global variable in function scope
+  console.log(x, "in function scope");
 }
+
+run();
+
+if (true) {
+  console.log(x, "in block scope");
+}
+
+// Function are block/function scope
+function add() {
+  const x = 50;
+  const y = 100;
+  console.log(x, "in function scope");
+  console.log(y + x, "in function scope");
+}
+
+add();
+
+// console.log(y); // Reference error
+
+// An if statement are block scope
+if (true) {
+  console.log(x);
+  const y = 200;
+  console.log(x + y, "in block scope");
+}
+
+// console.log(y); // Reference error
+
+// A for loop are block scope
+for (let i = 0; i < 5; i++) {
+  let y = 10;
+  console.log(i + y, "in block scope");
+}
+
+// console.log(i); // Reference error
+// console.log(y); // Reference error
