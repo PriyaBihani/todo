3492b1cfc82cd27f3ac8dded3025639ca0955b1d
PriyaBihani <priyabihani.official@gmail.com>
2023-10-25 23:51:39 +0530

    Code for types of loops

--- src/index.js
+++ src/index.js
@@ -1,46 +1,26 @@
-const age = 21;
-
-// if (age >= 18) {
-//   console.log("You are an adult");
-// } else {
-//   console.log("You are a minor");
-// }
-
-age >= 18 ? console.log("You are an adult") : console.log("You are a minor");
-
-const canVote = age >= 18 ? true : false;
-const canVote1 =
-  age >= 18
-    ? "You can vote"
-    : age < 12
-    ? "You are a child"
-    : "You are a teenager";
-console.log(canVote1);
-
-// For loops
-// for (let i = 0; i < 10; i++) {
-//   // block of code which will be repeated
-
-//   if (i === 7) {
-//     console.log("Number 7 is my favorite number");
-//   } else {
-//     console.log("Number", i);
-//   }
-// }
-
-// Nested loops
-
-// for (let i = 0; i < 10; i++) {
-//   console.log("Outer loop", i);
-
-//   for (let j = 0; j < 5; j++) {
-//     console.log(`${i} * ${j} = ${i * j}`);
-//   }
-// }
-
-// Looping through arrays
 const cars = ["Ford", "Chevy", "Honda", "Toyota"];
+const users = [{ name: "Priya" }, { name: "Riya" }, { name: "Deepak" }];
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
 
-for (let i = 0; i < cars.length; i++) {
-  console.log(cars[i]);
+// for .... of loop
+// for (const item of cars) {
+//   console.log(item);
+// }
+
+for (const user of users) {
+  console.log(user.name);
+}
+
+// for ... in loop
+const obj = {
+  color1: "red",
+  color2: "green",
+  color3: "blue",
+  color4: "yellow",
+};
+
+for (const key in obj) {
+  console.log(key, obj[key]);
 }
