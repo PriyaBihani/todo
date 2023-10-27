5acf0d3280a9a8237e0d81f226471518b7478662
PriyaBihani <priyabihani.official@gmail.com>
2023-10-27 20:53:56 +0530

    Code for dom in JS

--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.dir(window.document);
+console.log(document);
+console.log(window.document.body);
 
-// const doubleNumber = [];
+console.log(document.body.innerHTML);
+console.log(document.body.innerText);
 
-// numbers.forEach(function (num) {
-//   doubleNumber.push(num * 2);
-// });
-
-// numbers.forEach((num) => {
-//   doubleNumber.push(num * 2);
-// });
-
-const doubleNumbers = numbers.map((num) => "Number: " + num * 2);
-
-// Filters
-
-const evenNumbers = [];
-
-numbers.forEach((num) => {
-  if (num % 2 === 0) {
-    evenNumbers.push(num);
-  }
-});
-
-const evenNumbers2 = numbers.filter((num) => num % 2 === 0);
-
-// Reduce
-// let sum = 0;
-
-// numbers.forEach((num) => {
-//   sum += num;
-// });
-
-const sum = numbers.reduce((previous, current) => previous + current, 0);
-
-const doubleEvenNumbers = numbers
-  .filter((num) => num % 2 === 0)
-  .map((num) => num * 2)
-  .reduce((acc, cur) => acc + cur, 0);
-
-console.log(doubleEvenNumbers);
+document.body.innerHTML = "<h1>hello from index.j</h1>";
+console.log(document.body.innerHTML);
