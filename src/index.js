4211bd53c5d1a46bfac8f9d6da0276bb410b9967
PriyaBihani <priyabihani.official@gmail.com>
2023-10-29 01:52:14 +0530

    Code for shopping list setup

--- src/index.js
+++
@@ -1,9 +0,0 @@
-console.dir(window.document);
-console.log(document);
-console.log(window.document.body);
-
-console.log(document.body.innerHTML);
-console.log(document.body.innerText);
-
-document.body.innerHTML = "<h1>hello from index.j</h1>";
-console.log(document.body.innerHTML);
