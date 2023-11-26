064721b6372a72314b43b57cd1be3dd08c86d26d
PriyaBihani <priyabihani.official@gmail.com>
2023-11-27 01:20:01 +0530

    Code scripts in packageJSON

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-const http = require("http");
-const os = require("os");
-const events = require("events");
-const util = require("util");
-
-// fs.readFile("temp.txt", "utf8", (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
-
-// fs.writeFile(
-//   "newFile.txt",
-//   "Hey, this is written using writeFile cdsadasf",
-//   (err) => {
-//     if (err) throw err;
-//     console.log("File written successfully");
-//   }
-// );
-
-const data = fs.readFileSync("temp.txt", "utf8");
-console.log(data);
-
-fs.writeFileSync(
-  "newFile.txt",
-  "Hey, this is written using writeFile cdsadasf"
-);
-console.log("File written successfully");
-
-console.log("HERE");
+const { v4: uuidv4 } = require("uuid");
+console.log(uuidv4());
+console.log("hey there");
