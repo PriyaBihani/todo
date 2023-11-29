96b1b211edb986842e1a3b61716c89327f109c63
PriyaBihani <priyabihani.official@gmail.com>
2023-11-29 11:20:12 +0530

    code for api endpoints

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-// console.log(app);
+app.get("/", (req, res) => {
+  //   console.log(req);
+  res.status(700).send("Hello World from express");
+});
 
-app.get("/home", () => {
-  console.log("Hello World");
+app.get("/posts", (req, res) => {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.status(302).json(JSON.parse(data));
+  });
 });
 
 const PORT = 8000;
