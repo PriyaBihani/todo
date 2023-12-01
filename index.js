ffc670b07473d6ab6317e7139a14d9e92acbb964
PriyaBihani <priyabihani.official@gmail.com>
2023-12-02 00:11:38 +0530

    Code for Middlewares

--- index.js
+++ index.js
@@ -3,43 +3,40 @@ const fs = require("fs");
 
 const app = express();
 
+app.use(logger);
+
 app.get("/", (req, res) => {
-  //   console.log(req);
   res.status(700).send("Hello World from express");
 });
 
-// app.get("/myname", (req, res) => {
-//   res.send("Hello ");
-// });
-
-app.get("/myname/:name", function (req, res) {
-  console.log(req.params);
+app.get("/myname/:name", authMiddleware, function (req, res) {
   const { name } = req.params;
   res.status(200).send(`Hello my name is ${name}`);
 });
 
-app.get("/posts", (req, res) => {
-  console.log(req.query);
-  const { count } = req.query;
-  if (count) {
-    fs.readFile("./db.json", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      const posts = JSON.parse(data);
+app.get("/posts", authMiddleware, (req, res) => {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
 
-      res.status(302).json(posts.slice(0, count));
-    });
-  } else {
-    fs.readFile("./db.json", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-
-      res.status(302).json(JSON.parse(data));
-    });
-  }
+    res.status(302).json(JSON.parse(data));
+  });
 });
+
+function logger(req, res, next) {
+  console.log(req.method, req.url, new Date());
+  next();
+}
+
+function authMiddleware(req, res, next) {
+  if (req.query.admin === "true") {
+    console.log("I am authorised");
+    next();
+  } else {
+    res.status(401).send("You are not authorised");
+  }
+}
 
 const PORT = 8000;
 app.listen(8000, () => {
