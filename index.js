4c89556a6b54277daa21f4820dd2af3dbb04b234
PriyaBihani <priyabihani.official@gmail.com>
2023-12-03 19:54:35 +0530

    code for setup

--- index.js
+++ index.js
@@ -5,37 +5,31 @@ const app = express();
 
 app.use(logger);
 
-app.get("/", (req, res) => {
-  res.status(700).send("Hello World from express");
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
 });
 
-app.get("/myname/:name", authMiddleware, function (req, res) {
-  const { name } = req.params;
-  res.status(200).send(`Hello my name is ${name}`);
-});
-
-app.get("/posts", authMiddleware, (req, res) => {
-  fs.readFile("./db.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-
-    res.status(302).json(JSON.parse(data));
-  });
+app.get("/todos", async (req, res) => {
+  try {
+    const data = await fs.promises.readFile("./db.json", "utf8");
+    const parsedData = JSON.parse(data);
+    res.status(200).json({
+      status: "success",
+      msg: "Todos retrieved successfully",
+      data: parsedData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      msg: "Internal server error",
+      data: [],
+    });
+  }
 });
 
 function logger(req, res, next) {
   console.log(req.method, req.url, new Date());
   next();
-}
-
-function authMiddleware(req, res, next) {
-  if (req.query.admin === "true") {
-    console.log("I am authorised");
-    next();
-  } else {
-    res.status(401).send("You are not authorised");
-  }
 }
 
 const PORT = 8000;
