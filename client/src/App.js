f74484b11be00de7b937de1e159c7dd91d8d3ff0
PriyaBihani <priyabihani.official@gmail.com>
2023-12-19 18:48:49 +0530

    State in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import Footer from "./components/Footer";
 
-// const Greeting = ({ userName, age }) => {
-//   // console.log(props);
-//   return (
-//     <h1>
-//       Hello, {userName}. I am {age} years old
-//     </h1>
-//   );
-// };
-
-const Toggle = ({ isSwitchOn }) => {
-  return <div>{isSwitchOn ? <h1>ON</h1> : <h1>OFF</h1>}</div>;
-};
-
-const Button = ({ handleClick }) => {
-  return <button onClick={handleClick}>Click me</button>;
-};
-
 function App() {
-  const userName = "Deepak";
-  const age = 10;
+  const [count, setCount] = useState(0);
 
-  const isSwitchOn = false;
+  const handleInc = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
 
-  const handleClick = () => {
-    alert("clicked");
+  const handleDec = () => {
+    setCount((prevVal) => prevVal - 1);
+    console.log(count);
   };
 
   return (
     <div className="App">
-      {/* <Toggle isSwitchOn={isSwitchOn} /> */}
-
-      <Button handleClick={handleClick} />
-
-      {/* <Greeting userName={userName} age={age} /> */}
-      {/* <Header />
-      <TodoList />
-      <Footer /> */}
+      <button onClick={handleDec}> - </button>
+      <br />
+      {count} <br />
+      <button onClick={handleInc}> + </button>
     </div>
   );
 }
