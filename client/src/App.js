4da7aa9623e8841a38ad47c36b34ed5234045347
PriyaBihani <priyabihani.official@gmail.com>
2023-12-19 18:09:45 +0530

    Props in react

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,43 @@ import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import Footer from "./components/Footer";
 
+// const Greeting = ({ userName, age }) => {
+//   // console.log(props);
+//   return (
+//     <h1>
+//       Hello, {userName}. I am {age} years old
+//     </h1>
+//   );
+// };
+
+const Toggle = ({ isSwitchOn }) => {
+  return <div>{isSwitchOn ? <h1>ON</h1> : <h1>OFF</h1>}</div>;
+};
+
+const Button = ({ handleClick }) => {
+  return <button onClick={handleClick}>Click me</button>;
+};
+
 function App() {
+  const userName = "Deepak";
+  const age = 10;
+
+  const isSwitchOn = false;
+
+  const handleClick = () => {
+    alert("clicked");
+  };
+
   return (
     <div className="App">
-      <Header />
+      {/* <Toggle isSwitchOn={isSwitchOn} /> */}
+
+      <Button handleClick={handleClick} />
+
+      {/* <Greeting userName={userName} age={age} /> */}
+      {/* <Header />
       <TodoList />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
