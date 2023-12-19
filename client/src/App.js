7830ac800b6e152fddd050934600eb9a4f9c910b
PriyaBihani <priyabihani.official@gmail.com>
2023-12-19 14:38:37 +0530

    code for components and jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
 function App() {
-  return <div className="App">Hello World how are you doing?</div>;
+  const name = "Kartik";
+  const age = 20;
+  const favouriteFoods = ["Pizza", "Burger", "Pasta"];
+  const hobbies = ["Reading", "Writing", "Coding"];
+  const showHobbies = true;
+  return (
+    <div className="App">
+      Hello World how are you doing?
+      <p> Hey, I am {name} and I am doing great</p>
+      <p> I am {age} years old</p>
+      <ul>
+        My favourite foods are:
+        {favouriteFoods.map((food) => (
+          <li>{food}</li>
+        ))}
+      </ul>
+      {showHobbies && (
+        <ul>
+          My hobbies are:
+          {hobbies.map((hobby) => (
+            <li>{hobby}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
