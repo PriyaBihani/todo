f7ea78e2c3a25abdc0b1eba7ae0fd6d40dcdc130
PriyaBihani <priyabihani.official@gmail.com>
2023-12-19 15:30:39 +0530

    Code for jsx attributes and event handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 function App() {
   const name = "Kartik";
   const age = 20;
-  const favouriteFoods = ["Pizza", "Burger", "Pasta"];
+  const favouriteFoods = ["Pizza", "Burger", "Pasta", "Noodles"];
   const hobbies = ["Reading", "Writing", "Coding"];
-  const showHobbies = true;
+  const showHobbies = false;
+
+  const handleClick = (e) => {
+    console.log("tis is clicked");
+    console.log(e);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        color: "blue",
+        backgroundColor: "seagreen",
+        fontFamily: showHobbies ? "cursive" : "monospace",
+      }}
+    >
       Hello World how are you doing?
+      <br />
+      <input onChange={handleChange} />
+      <button onClick={handleClick}>Click</button>
       <p> Hey, I am {name} and I am doing great</p>
       <p> I am {age} years old</p>
-      <ul>
+      <ul className="list">
         My favourite foods are:
         {favouriteFoods.map((food) => (
           <li>{food}</li>
